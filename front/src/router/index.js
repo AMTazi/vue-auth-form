2244add4190abaa92d8506e2419aa343adbf515d
AMTazi <taziabdelmounaim@gmail.com>
2019-10-31 18:03:14 +0100

    Hotfix: change production vue router mode to hash

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -45,7 +45,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: process.env.NODE_ENV === "production" ? "hash" : "history",
   base: process.env.BASE_URL,
   routes
 });
